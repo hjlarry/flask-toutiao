a6b031d579f841c8a1768540a1cf5a5d73d28c5a
hjlarry <hjlarry@163.com>
2018-12-06 14:50:38 +0800

    fix password reset landing page

--- views/account.py
+++ views/account.py
@@ -6,8 +6,19 @@ from models.user import User
 bp = Blueprint("account", __name__)
 
 
-@bp.route("landing")
-def landing():
-    return render_template("security/landing.html")
+@bp.route("register_landing")
+def register_landing():
+    """注册后跳转页面"""
+    return render_template("security/register_landing.html")
 
 
+@bp.route("reset_landing")
+def reset_landing():
+    """重置密码后跳转页面"""
+    return render_template("security/reset_landing.html")
+
+
+@bp.route("confirm_landing")
+def confirm_landing():
+    """在邮件中点击确认链接后跳转页面"""
+    return render_template("security/confirm_landing.html")

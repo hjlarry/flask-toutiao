9c61564af69ca46a8f5ca4b0d52a7a331a3ea8dc
hjlarry <hjlarry@163.com>
2018-12-06 14:24:04 +0800

    complete app and reset,forget,change password logic

--- app.py
+++ app.py
@@ -1,32 +1,65 @@
+from flask import render_template
+from flask_security import current_user
+
 import config
 from views.index import bp as index_bp
 from views.account import bp as account_bp
 from corelib.exmail import send_mail
-from corelib.flask import Flask
+from corelib.flask_ import Flask
 from ext import db, security, mail, debug_bar
 from forms import ExtendedRegisterForm, ExtendedLoginForm
 from models.user import user_datastore
 
 
 def create_app():
-
     app = Flask(__name__)
     app.config.from_object(config)
+    init_app(app)
+    register_bp(app)
+    app.context_processor(inject_template_global)
+
+    @app.teardown_request
+    def teardown_request(exception):
+        if exception:
+            db.session.rollback()
+        db.session.remove()
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template("404.html"), 404
+
+    return app
+
+
+def init_app(app):
     db.init_app(app)
     mail.init_app(app)
     debug_bar.init_app(app)
-
     _state = security.init_app(
-        app, user_datastore, confirm_register_form=ExtendedRegisterForm, login_form=ExtendedLoginForm
+        app,
+        user_datastore,
+        confirm_register_form=ExtendedRegisterForm,
+        login_form=ExtendedLoginForm,
     )
     security._state = _state
     app.security = security
     security.send_mail_task(send_mail)
 
+
+def register_bp(app):
     app.register_blueprint(index_bp, url_prefix="/")
     app.register_blueprint(account_bp, url_prefix="/")
 
-    return app
+
+def inject_template_global():
+    return {
+        "isinstance": isinstance,
+        "current_user": current_user,
+        "getattr": getattr,
+        "hasattr": hasattr,
+        "dir": dir,
+        "len": len,
+    }
 
 
 app = create_app()

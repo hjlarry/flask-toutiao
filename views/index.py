214858a8e10130b37147abb0ad9cadca86367cfd
hjlarry <hjlarry@163.com>
2018-12-07 11:49:39 +0800

    fix avatar upload

--- views/index.py
+++ views/index.py
@@ -1,15 +1,21 @@
 from flask.blueprints import Blueprint
-from flask import render_template
+from flask import render_template, send_from_directory
 from flask_security import current_user
 
 from models.core import Post
+from config import UPLOAD_FOLDER
 
 bp = Blueprint("index", __name__)
 
 
 @bp.route("/")
 def index():
-    return render_template("index.html", user=current_user)
+    return render_template("index.html")
+
+
+@bp.route("/static/avatars/<path>")
+def avatars(path):
+    return send_from_directory(UPLOAD_FOLDER / "avatars", path)
 
 
 @bp.route("/post/<int:id>")

d24ba23477bd2edb0d62716706025581b323fa3e
hjlarry <hjlarry@163.com>
2018-12-17 16:26:00 +0800

    fix bug

--- corelib/mc.py
+++ corelib/mc.py
@@ -1,4 +1,3 @@
-import re
 import inspect
 import functools
 from pickle import UnpicklingError
@@ -6,53 +5,14 @@ from pickle import UnpicklingError
 from sqlalchemy.ext.serializer import loads, dumps
 
 from corelib.db import rdb
-from corelib.utils import Empty
+from corelib.utils import Empty, empty
 
-
-__formaters = {}
-percent_pattern = re.compile(r"%\w")
-brace_pattern = re.compile(r"\{[\w\d\[\]_]+\}")
-
-
-def formatter(text):
-    """
-    >>> format('%s %s', 3, 2, 7, a=7, id=8)
-    '3 2'
-    >>> format('%(a)d %(id)s', 3, 2, 7, a=7, id=8)
-    '7 8'
-    >>> format('{1} {id}', 3, 2, a=7, id=8)
-    '2 8'
-    >>> class Obj: id = 3
-    >>> format('{obj.id} {0.id}', Obj(), obj=Obj())
-    '3 3'
-    >>> class Obj: id = 3
-    >>> format('{obj.id.__class__} {obj.id.__class__.__class__} {0.id} {1}', \
-    >>> Obj(), 6, obj=Obj())
-    "<type 'int'> <type 'type'> 3 6"
-    """
-    percent = percent_pattern.findall(text)
-    brace = brace_pattern.search(text)
-    if percent and brace:
-        raise Exception("mixed format is not allowed")
-    if percent:
-        n = len(percent)
-        return lambda *a, **kw: text % tuple(a[:n])
-    elif "%(" in text:
-        return lambda *a, **kw: text % kw
-    else:
-        return text.format
-
-
-def format(text, *a, **kw):
-    f = __formaters.get(text)
-    if f is None:
-        f = formatter(text)
-        __formaters[text] = f
-    return f(*a, **kw)
+BUILTIN_TYPES = (int, bytes, str, float, bool)
 
 
 def gen_key_factory(key_pattern, arg_names, defaults):
     args = dict(zip(arg_names[-len(defaults) :], defaults)) if defaults else {}
+
     if callable(key_pattern):
         names = inspect.getargspec(key_pattern)[0]
 
@@ -63,7 +23,7 @@ def gen_key_factory(key_pattern, arg_names, defaults):
         if callable(key_pattern):
             key = key_pattern(*[aa[n] for n in names])
         else:
-            key = format(key_pattern, *[aa[n] for n in arg_names], **aa)
+            key = key_pattern.format(*[aa[n] for n in arg_names], **aa)
         return key and key.replace(" ", "_"), aa
 
     return gen_key
@@ -83,15 +43,19 @@ def cache(key_pattern, expire=None):
                 return f(*a, **kw)
             force = kw.pop("force", False)
             r = rdb.get(key) if not force else None
-
             if r is None:
                 r = f(*a, **kw)
                 if r is not None:
-                    r = dumps(r)
+                    if not isinstance(r, BUILTIN_TYPES):
+                        r = dumps(r)
                     rdb.set(key, r, expire)
+                else:
+                    r = dumps(empty)
+                    rdb.set(key, r, expire)
+
             try:
                 r = loads(r)
-            except (TypeError, UnpicklingError, EOFError):
+            except (TypeError, UnpicklingError):
                 pass
             if isinstance(r, Empty):
                 r = None
